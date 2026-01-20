e74e002b894f019b7b690a9c8be55d8692a8af5d
Lauri Jutila <git@laurijutila.com>
2026-01-20 13:57:38 +0000

    Bump version to 1.0.1

--- lib/sec_api/version.rb
+++ lib/sec_api/version.rb
@@ -31,7 +31,7 @@ module SecApi
     MINOR = 0
 
     # @return [Integer] Patch version number (bug fixes)
-    PATCH = 0
+    PATCH = 1
 
     # @return [String, nil] Pre-release identifier (e.g., "alpha", "beta", "rc1")
     PRE = nil
